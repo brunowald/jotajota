ff6f6554c25ffefdbf3f72e808f7d0c0976f4ded
brunowald <brunowald@gmail.com>
2025-10-14 17:43:19 -0300

    bold para asociate

--- src/app/comunidad/page.tsx
+++ src/app/comunidad/page.tsx
@@ -78,7 +78,7 @@ export default async function CommunityPage() {
           <div className="col-lg-6">
             <div className="card shadow-lg border-primary h-100">
               <div className="card-body d-flex flex-column justify-content-between">
-                <h3 className="h5 text-center text-info mb-4">ASOCIATE</h3>
+                <h3 className="h2 fw-bold text-center text-info mb-4">ASOCIATE</h3>
                 <AsociateTabs
                   mensualOptions={mensualOptions}
                   anualOptions={anualOptions}
